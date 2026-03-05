f5cb8b2a5b74f5e0b6ef312253dc39263eaa5049
jkob-git <247120541+jkob-git@users.noreply.github.com>
2026-03-05 19:26:09 +0800

    Delete KainKlinik directory

--- KainKlinik/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import KainKlinikal from "./KainKlinikal";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <KainKlinikal />
-  </React.StrictMode>
-);
